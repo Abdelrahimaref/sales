a471560710cd08ce00fbc49207a62e5bf101783c
Abdo <abdoaref163@gmail.com>
2020-06-07 13:15:01 +0200

    rrr

--- community/js/e.js
+++ community/js/e.js
@@ -1,3 +1,184 @@
+/********** technology page  *******/
+$('.data-btn').click(function(){
+  setTimeout(function(){
+    $('.for-design').fadeOut(300);
+    $('.for-workflow').fadeOut(300);
+    $('.data-btn').addClass('icon-card--active');
+    $('.design-btn').removeClass('icon-card--active');
+    $('.workflow-btn').removeClass('icon-card--active');
+    $('.data-btn img').attr("src" , "images/white-1.svg");
+    $('.design-btn img').attr("src" , "images/light-2.svg");    
+    $('.workflow-btn img').attr("src" , "images/light-3.svg");
+
+  },400)
+  setTimeout(function(){
+    $('.for-data').fadeIn(800);
+  },700)
+});
+$('.design-btn').click(function(){
+  setTimeout(function(){
+    $('.for-data').fadeOut(300);
+    $('.for-workflow').fadeOut(300);
+    $('.design-btn').addClass('icon-card--active');
+    $('.data-btn').removeClass('icon-card--active');
+    $('.workflow-btn').removeClass('icon-card--active');
+    $('.data-btn img').attr("src" , "images/light-1.svg");
+    $('.design-btn img').attr("src" , "images/white-2.svg");    
+    $('.workflow-btn img').attr("src" , "images/light-3.svg");
+  },400)
+  setTimeout(function(){
+    $('.for-design').fadeIn(800);
+  },700)
+});
+$('.workflow-btn').click(function(){
+  setTimeout(function(){
+    $('.for-data').fadeOut(300);
+    $('.for-design').fadeOut(300);
+    $('.workflow-btn').addClass('icon-card--active');
+    $('.design-btn').removeClass('icon-card--active');
+    $('.data-btn').removeClass('icon-card--active');
+    $('.data-btn img').attr("src" , "images/light-1.svg");
+    $('.design-btn img').attr("src" , "images/light-2.svg");    
+    $('.workflow-btn img').attr("src" , "images/white-3.svg");
+
+  },400)
+  setTimeout(function(){
+    $('.for-workflow').fadeIn(800);
+  },700)
+});
+
+/********** QUESTION PAGE  *******/
+var que = 1 ; 
+$('.question-box').click(function(){
+  if(que==1) {
+    $(this).addClass('card');
+    $(this).children('.answer').fadeIn();
+    que=2 ; 
+  }
+  else if(que==2) {
+    $(this).removeClass('card');
+    $(this).children('.answer').fadeOut();
+    que=1 ; 
+  }
+
+});
+      /****  Questions Type  ****/
+$('.category-question').click(function(){
+  $('.category-questions').fadeIn();
+  $('.start-questions').fadeOut();
+  $('.native-questions').fadeOut();
+  $('.pricing-questions').fadeOut();
+  $('.support-questions').fadeOut();
+  $(this).addClass('active-color');
+  $('.start-question').removeClass('active-color');
+  $('.native-question').removeClass('active-color');
+  $('.pricing-question').removeClass('active-color');
+  $('.support-question').removeClass('active-color');
+})
+$('.start-question').click(function(){
+  $('.start-questions').fadeIn();
+  $('.category-questions').fadeOut();
+  $('.native-questions').fadeOut();
+  $('.pricing-questions').fadeOut();
+  $('.support-questions').fadeOut();
+  $(this).addClass('active-color');
+  $('.category-question').removeClass('active-color');
+  $('.native-question').removeClass('active-color');
+  $('.pricing-question').removeClass('active-color');
+  $('.support-question').removeClass('active-color');
+})
+$('.native-question').click(function(){
+  $('.native-questions').fadeIn();
+  $('.category-questions').fadeOut();
+  $('.start-questions').fadeOut();
+  $('.pricing-questions').fadeOut();
+  $('.support-questions').fadeOut();
+  $(this).addClass('active-color');
+  $('.start-question').removeClass('active-color');
+  $('.category-question').removeClass('active-color');
+  $('.pricing-question').removeClass('active-color');
+  $('.support-question').removeClass('active-color');
+})
+$('.pricing-question').click(function(){
+  $('.pricing-questions').fadeIn();
+  $('.category-questions').fadeOut();
+  $('.start-questions').fadeOut();
+  $('.native-questions').fadeOut();
+  $('.support-questions').fadeOut();
+  $(this).addClass('active-color');
+  $('.start-question').removeClass('active-color');
+  $('.native-question').removeClass('active-color');
+  $('.category-question').removeClass('active-color');
+  $('.support-question').removeClass('active-color');
+})
+$('.support-question').click(function(){
+  $('.support-questions').fadeIn();
+  $('.start-questions').fadeOut();
+  $('.native-questions').fadeOut();
+  $('.pricing-questions').fadeOut();
+  $('.category-questions').fadeOut();
+  $(this).addClass('active-color');
+  $('.start-question').removeClass('active-color');
+  $('.native-question').removeClass('active-color');
+  $('.pricing-question').removeClass('active-color');
+  $('.category-question').removeClass('active-color');
+})
+
+
+/*********** End QUESTION PAGE  *******/
+
+
+/*********** team card in about page *********/
+var f = 1 ; 
+$('.post-all').hover(function(){
+  if(f==1) {
+    $(this).children().children('.post-image-box').children('img').css({width:'80px',height:'80px'})
+    $(this).children().children('.all-con').fadeOut();
+    $(this).children().children('.post-subname2').fadeIn();
+    $(this).css({backgroundColor:'#f5f9fa'});
+    f=2 ; 
+  }
+  else if(f==2) {
+    $(this).children().children('.post-image-box').children('img').css({width:'180px',height:'180px'})
+    $(this).children().children('.all-con').fadeIn();
+    $(this).children().children('.post-subname2').fadeOut();
+    $(this).css({backgroundColor:'#FFF'});
+    f=1;
+  }
+});
+
+$('.team-btn-1').click(function(){
+  $('.team-1').fadeIn();
+  $('.team-2').css({display:'none'})
+});
+$('.team-btn-2').click(function(){
+  $('.team-2').css({display:'block'})
+  $('.team-1').fadeOut();
+})
+
+
+/********   1  - sub card  ******** */ 
+
+$('.sub-card').click(function(){
+  if($(this).hasClass('sub')) {
+    $(this).hide("slide", {direction: "right"}, 150);
+    $('.main-card').hide("slide", {direction: "left"}, 150);
+    $(this).show("slide", {direction: "right"}, 150);
+    $('.main-card').show("slide", {direction: "left"}, 150);
+  }
+  $(this).addClass('color-shadow').removeClass('sub').removeClass('border-main');
+  $('.main-card').addClass('border-main').addClass('sub2').removeClass('color-shadow');
+})
+$('.main-card').click(function(){
+  if($(this).hasClass('sub2')) {
+    $(this).hide("slide", {direction: "left"}, 150);
+    $('.sub-card').hide("slide", {direction: "right"}, 150);
+    $(this).show("slide", {direction: "left"}, 150);
+    $('.sub-card').show("slide", {direction: "right"}, 150);
+  }
+  $(this).addClass('color-shadow').removeClass('sub2').removeClass('border-main');  
+  $('.sub-card').addClass('border-main').addClass('sub').removeClass('color-shadow');
+})
 /**********  Payment Box   ***********/
 
 $('.pay11').click(function(){
@@ -103,6 +284,24 @@ $(window).on("scroll",function(){
     }
       else {
         $('.nav-scroll').css({
+          backgroundColor:'transparent',
+          boxShadow:'none',
+
+        });
+      }
+}) ;
+
+/**** For Navbar in about page ****/
+
+$(window).on("scroll",function(){
+  if($(window).scrollTop()>=100) {
+      $('.nav-scroll2').css({
+        backgroundColor:'#FFF',
+        boxShadow:'4px 4px 3px #F5F5F5',
+      });
+    }
+      else {
+        $('.nav-scroll2').css({
           backgroundColor:'#FFF',
           boxShadow:'none',
 
@@ -240,24 +439,11 @@ $(document).ready(function(){
 });
 
 
-var real = document.getElementById('real-life');
-var btn  = document.getElementById('choose-btn-pc');
-var customTxt  = document.getElementById('customTxt');
-var srcBtn = document.getElementById('src-btn');
-var fileBtn = document.getElementById('file-btn');
+var srcBtn = document.getElementById('src-btn2');
+var fileBtn = document.getElementById('file-btn2');
 var customTxt2 = document.getElementById('customTxt2');
 
 
-btn.addEventListener("click",function() {
-  real.click();
-});
-real.addEventListener("change",function() {
-  if(real.value) {
-    customTxt.innerHTML = real.value;
-  } else {
-    customTxt.innerHTML = "No File Chosen yet !";
-  }
-});
 
 srcBtn.addEventListener("click",function() {
   fileBtn.click();
@@ -269,9 +455,5 @@ fileBtn.addEventListener("change",function() {
     customTxt2.innerHTML = "No File Chosen yet !";
   }
 })
-
-
-
-/**** Course Content Section  ****/
 
 
